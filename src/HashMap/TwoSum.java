916875d0714f9c52f25d59e3d41516a34b04d93e
dhruv264965 <dhruv264965shukla@gmail.com>
2024-09-29 01:09:46 +0530

    commit

---
+++ src/HashMap/TwoSum.java
@@ -0,0 +1,49 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int ans[]=new int[2];
+        ans[0]=ans[1]=-1;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int parter = target - nums[i];
+            if(mp.containsKey(parter)){
+                ans[0]=mp.get(parter);
+                ans[1]=i;
+                return ans;
+
+            }
+            mp.put(nums[i],i);
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+       // int n = 5;
+        int[] nums = {2,152,4,7};
+        int target = 9;
+        int[] ans=twoSum(nums,target);
+        System.out.println("["+ans[0]+""+ans[1]+"]");
+
+
+
+    }
+}
+//        int temp[]=new int[2];
+//        for(int i=0;i<nums.length;i++)
+//        {
+//            for(int j=i+1;j<nums.length;j++)
+//            {
+//                if(nums[i]+nums[j]==target)
+//                {
+//                    temp[0]=i;
+//                    temp[1]=j;
+//                }
+//            }
+//        }
+//        return temp;
+//    }
